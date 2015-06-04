e8394f150301c3219c492311528fa7030b6c5b52
MKHenson <mat@webinate.net>
2015-06-04 20:22:09 +0100

    Updated shell script

--- install-script.sh
+++ install-script.sh
@@ -9,17 +9,50 @@ nvm_latest_version() {
   echo "v0.0.1"
 }
 
+echo "cleaning up folder..."
+
+# Remove node modules
+if [ -d "node_modules" ]; then
+	rm node_models -R
+fi
+
+# Remove views
+if [ -d "views" ]; then
+	rm views -R
+fi
+
+# Remove resources
+if [ -d "resources" ]; then
+	rm resources -R
+fi
+
+# Remove lib
+if [ -d "lib" ]; then
+	rm lib -R
+fi
+
 echo "Downloading latest version from github $(nvm_latest_version)"
 
 #download latest
 wget https://github.com/MKHenson/modepress/archive/master.zip
-unzip -o -j "master.zip" "webinate-users-master/server/*"
-if [ -d "node_modules" ]; then
-	rm node_models -R
+unzip -o "master.zip" "modepress-master/*"
+
+# Moves the server folder to the current directory
+mv modepress-master/server/* .
+
+# Remove modepress-master
+if [ -d "modepress-master" ]; then
+	rm modepress-master -R
 fi
+
+# Remove the zip file
 rm master.zip
+
+# Copy the example config to a config.json
 cp "example-config.json" "config.json"
-echo "Users successfully installed"
+
+# All done
+echo "Modepress successfully installed"
 echo "Please run an NPM update and edit the config.json"
 exit
 } # this ensures the entire script is downloaded #
