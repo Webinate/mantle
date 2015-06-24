1e4d9141105f613d3a96631bea5d6115c2e9572b
MKHenson <mat@webinate.net>
2015-06-24 14:22:08 +0100

    Added support for user specified thread counts

--- src-server/main.ts
+++ src-server/main.ts
@@ -1,9 +1,40 @@
 ﻿import * as cluster from "cluster";
 import * as os from "os";
+import * as yargs from "yargs";
 
+var arguments = yargs.argv;
 var numCPUs = os.cpus().length;
 
-if (cluster.isMaster)
+// Check for the threads argument
+if (arguments.numThreads)
+{
+    if (arguments.numThreads == "max")
+    {
+        console.log(`Setting the number of clusters to  ${numCPUs}`);
+    }
+    else if (isNaN(parseInt(arguments.numThreads)))
+    {
+        console.log("attribute numThreads must be a number");
+        process.exit();
+    }
+    else if (arguments.numThreads > numCPUs)
+    {
+        console.log(`You only have ${numCPUs} threads available - attribute numThreads will be set to ${numCPUs}`);
+    }
+    else if (arguments.numThreads)
+    {
+        console.log(`Setting the number of clusters to  ${arguments.numThreads}`);
+        numCPUs = arguments.numThreads;
+    }
+}
+
+// Run as a single cluster
+if (numCPUs == 1)
+{
+    console.log(`Running as single cluster`);
+    require("./Startup.js");
+}
+else if (cluster.isMaster)
 {
     // Fork workers.
     for (var i = 0; i < numCPUs; i++)
