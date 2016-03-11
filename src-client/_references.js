72b6974792a4b0c92321adac58bad76ff3bb681f
Mathew Henson <mat@webinate.net>
2016-03-11 09:57:02 +0000

    Removed unused js files

--- src-client/_references.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="../src-interface/interfaces.d.ts" />
-/// <reference path="lib/definitions/jquery.d.ts" />
-/// <reference path="lib/definitions/angular.d.ts" />
-/// <reference path="lib/definitions/angular-ui-router.d.ts" />
-/// <reference path="lib/Config.ts" />
-/// <reference path="lib/Application.ts" /> 
-//# sourceMappingURL=_references.js.map
