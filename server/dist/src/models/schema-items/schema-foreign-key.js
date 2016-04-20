6a9ce74da4a8933de4f380db90ad6f107ea3378f
Mathew Henson <mat@webinate.net>
2016-04-21 00:07:44 +0100

    Added foreign key schema item. WIP

---
+++ server/dist/src/models/schema-items/schema-foreign-key.js
@@ -0,0 +1,72 @@
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var schema_item_1 = require("./schema-item");
+var mongodb_1 = require("mongodb");
+var utils_1 = require("../../utils");
+/**
+ * Represents a mongodb ObjectID of a document in separate collection.
+ * Foreign keys are used as a way of relating models to one another. They can be required or optional.
+ * Required keys will mean that the current document cannot exist if the target does not. Optional keys
+ * will simply be nullified if the target no longer exists.
+ */
+var SchemaForeignKey = (function (_super) {
+    __extends(SchemaForeignKey, _super);
+    /**
+    * Creates a new schema item
+    * @param {string} name The name of this item
+    * @param {string} val The string representation of the foreign key's _id
+    * @param {string} targetCollection The name of the collection to which the target exists
+    */
+    function SchemaForeignKey(name, val) {
+        _super.call(this, name, val);
+    }
+    /**
+    * Creates a clone of this item
+    * @returns {SchemaId} copy A sub class of the copy
+    * @returns {SchemaForeignKey}
+    */
+    SchemaForeignKey.prototype.clone = function (copy) {
+        copy = copy === undefined ? new SchemaForeignKey(this.name, this.value) : copy;
+        _super.prototype.clone.call(this, copy);
+        return copy;
+    };
+    /**
+    * Checks the value stored to see if its correct in its current form
+    * @returns {boolean | string} Returns true if successful or an error message string if unsuccessful
+    */
+    SchemaForeignKey.prototype.validate = function () {
+        var transformedValue = this.value;
+        if (typeof this.value == "string") {
+            if (utils_1.Utils.isValidObjectID(this.value))
+                transformedValue = this.value = new mongodb_1.ObjectID(this.value);
+            else if (this.value.trim() != "")
+                return "Please use a valid ID for '" + this.name + "'";
+            else
+                transformedValue = null;
+        }
+        if (!transformedValue) {
+            this.value = null;
+            return true;
+        }
+        if (!transformedValue)
+            return "Please use a valid ID for '" + this.name + "'";
+        else
+            return true;
+    };
+    /**
+    * Gets the value of this item
+    * @returns {SchemaValue}
+    */
+    SchemaForeignKey.prototype.getValue = function () {
+        if (!this.value)
+            return null;
+        else
+            return this.value;
+    };
+    return SchemaForeignKey;
+}(schema_item_1.SchemaItem));
+exports.SchemaForeignKey = SchemaForeignKey;
---
+++ server/src/models/schema-items/schema-foreign-key.ts
@@ -0,0 +1,77 @@
+﻿import {SchemaItem} from "./schema-item";
+import {ObjectID} from "mongodb";
+import {Utils} from "../../utils"
+
+/**
+ * Represents a mongodb ObjectID of a document in separate collection.
+ * Foreign keys are used as a way of relating models to one another. They can be required or optional.
+ * Required keys will mean that the current document cannot exist if the target does not. Optional keys
+ * will simply be nullified if the target no longer exists.
+ */
+export class SchemaForeignKey extends SchemaItem<ObjectID | string>
+{
+	/**
+	* Creates a new schema item
+	* @param {string} name The name of this item
+	* @param {string} val The string representation of the foreign key's _id
+    * @param {string} targetCollection The name of the collection to which the target exists
+	*/
+    constructor(name: string, val: string)
+    {
+        super(name, val);
+    }
+
+	/**
+	* Creates a clone of this item
+	* @returns {SchemaId} copy A sub class of the copy
+	* @returns {SchemaForeignKey}
+	*/
+    public clone(copy?: SchemaForeignKey): SchemaForeignKey
+    {
+        copy = copy === undefined ? new SchemaForeignKey(this.name, <string>this.value) : copy;
+        super.clone(copy);
+		return copy;
+	}
+
+	/**
+	* Checks the value stored to see if its correct in its current form
+	* @returns {boolean | string} Returns true if successful or an error message string if unsuccessful
+	*/
+	public validate(): boolean | string
+    {
+        var transformedValue = this.value;
+
+        if (typeof this.value == "string")
+        {
+            if (Utils.isValidObjectID(<string>this.value))
+                transformedValue = this.value = new ObjectID(<string>this.value);
+            else if ((<string>this.value).trim() != "")
+                return `Please use a valid ID for '${this.name}'`;
+            else
+                transformedValue = null;
+        }
+
+        if (!transformedValue)
+        {
+            this.value = null;
+            return true;
+        }
+
+        if (!transformedValue)
+            return `Please use a valid ID for '${this.name}'`;
+		else
+			return true;
+    }
+
+    /**
+	* Gets the value of this item
+    * @returns {SchemaValue}
+	*/
+    public getValue(): ObjectID
+    {
+        if (!this.value)
+            return null;
+        else
+            return <ObjectID>this.value;
+    }
+}
