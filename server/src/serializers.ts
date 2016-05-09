2c60ec26aef542d8e4bba732dcb4a45ddf468397
Mathew Henson <mat@webinate.net>
2016-05-09 12:04:01 +0100

    Updated config to ES6 and added a helper function file for serialization

---
+++ server/src/serializers.ts
@@ -0,0 +1,28 @@
+"use strict";
+
+import express = require("express");
+import * as http from "http";
+import * as mp from "modepress-api";
+import * as winston from "winston";
+
+/**
+ * Helper function to return a status 200 json object of type T
+ */
+export function okJson<T extends mp.IResponse>( data: T, res: express.Response )
+{
+    if (data.error)
+        winston.error(data.message, { process: process.pid });
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end( JSON.stringify( data ) );
+}
+
+/**
+ * Helper function to return a status 200 json object of type T
+ */
+export function errJson( err: Error, res: express.Response )
+{
+    winston.error(err.message, { process: process.pid });
+    res.setHeader('Content-Type', 'application/json');
+    res.end( JSON.stringify( <mp.IResponse>{ error : true, message: err.message } ) );
+}
