4b81770ce9f960851a243b0f6a60141efb791243
Mathew Henson <mat@webinate.net>
2016-10-17 11:55:06 +0100

    Gulp cleanup
    * Created new tasks for updating semvar tags
    * Created separate setup file
    * Standardised version in all packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require( 'gulp' );
 var ts = require( 'gulp-typescript' );
 var tslint = require( 'gulp-tslint' );
-var download = require( 'gulp-download' );
-var rename = require( "gulp-rename" );
+var setup = require( './gulp/setup.js' );
 
 // CONFIG
 // ==============================
 const tsProject = ts.createProject( 'tsconfig.json' );
+const configFiles = [
+    './readme.md',
+    './install-script.sh',
+    './test/package.json',
+    './src/dist-files/package.json',
+    './package.json'
+];
 
-
-// Builds each of the ts files into JS files in the output folder
+/**
+ * Builds each of the ts files into JS files in the output folder
+ */
 gulp.task( 'ts-code', function() {
     var tsResult = tsProject.src()
         .pipe( tsProject() );
@@ -32,44 +39,32 @@ gulp.task( 'tslint', [ 'ts-code' ], function() {
 });
 
 /**
- * This function downloads a definition file from github and writes it to a destination
- * @param {string} url The url of the file to download
- * @param {string} dest The destination folder to move the file to
- */
-function getDefinition( url, dest, name ) {
-    return new Promise( function( resolve, reject ) {
-        download( url )
-            .pipe( rename( name ) )
-            .pipe( gulp.dest( dest ) )
-            .on( 'error', function( err ) {
-                throw ( err )
-            })
-            .on( 'end', function() {
-                resolve( true );
-            })
-    });
-}
-
-/**
  * Downloads the definition files used in the development of the application and moves them into the definitions folder
  */
-gulp.task( 'install', function() {
+gulp.task( 'install-definitions', function() {
     return Promise.all( [
-        getDefinition( "https://raw.githubusercontent.com/MKHenson/users/dev/src/definitions/generated/users.d.ts", "src/definitions/required/", "users.d.ts" )
+        setup.getDefinition( "https://raw.githubusercontent.com/MKHenson/users/dev/src/definitions/generated/users.d.ts", "src/definitions/required/", "users.d.ts" )
     ] );
 });
 
-// Copies the distribution files from src to the dist folder
+/**
+ * Copies the distribution files from src to the dist folder
+ */
 gulp.task( 'dist-files', function() {
-
     return gulp.src( [ 'src/dist-src/*.json', 'src/dist-src/modepress-api/*.json' ], { base: "src/dist-src/" })
         .pipe( gulp.dest( './dist' ) );
 });
 
-// Copies the modepress definition into a definitions/generated folder
+/**
+ * Copies the modepress definition into a definitions/generated folder
+ */
 gulp.task( 'ts-code-definitions', function() {
     return gulp.src( [ 'src/definitions/custom/modepress.d.ts' ], { base: 'src/definitions/custom/' })
         .pipe( gulp.dest( './src/definitions/generated' ) );
 });
 
+gulp.task( 'bump-patch', function() { return setup.bumpVersion( setup.bumpPatchNum, configFiles ) });
+gulp.task( 'bump-minor', function() { return setup.bumpVersion( setup.bumpMidNum, configFiles ) });
+gulp.task( 'bump-major', function() { return setup.bumpVersion( setup.bumpMajorNum, configFiles ) });
+gulp.task( 'install', [ 'install-definitions' ] );
 gulp.task( 'build', [ 'ts-code', 'dist-files', 'ts-code-definitions' ] );
