11290848ef69e978bb56114263254d1dc1b28ac4
Mathew Henson <mat@webinate.net>
2016-10-18 09:19:40 +0100

    Generated logs for v0.5.0

--- git-logs.js
+++ git-logs.js
@@ -3,8 +3,8 @@ var fs = require( 'fs' )
 var exec = require( 'child_process' ).exec;
 var child;
 
-var prevTag = "v0.4.2";
-var nextTag = "v0.4.3";
+var prevTag = "v0.4.3";
+var nextTag = "v0.5.0";
 
 
 // Executes the git log command
