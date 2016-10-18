5df2471efa27d7e74c0550b8d0a1b9ea45de3cf9
Mathew Henson <mat@webinate.net>
2016-10-18 10:47:15 +0100

    v0.5.2

--- install-script.sh
+++ install-script.sh
@@ -6,7 +6,7 @@ set -e
 
 # Functiom that prints the latest stable version
 version() {
-  echo "0.5.1"
+  echo "0.5.2"
 }
 
 echo "Downloading latest version from github $(version)"
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 A simple Mongo-Node CMS server. The server is accessed via a RESTful API.
 
 ## Current stable version
-* v0.5.1
+* v0.5.2
 
 ## Requirements
 * MongoDB v3
