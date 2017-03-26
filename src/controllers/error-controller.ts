74b3c9e302368ec8c0adff43804311a4da86e0ab
Mathew Henson <mat@webinate.net>
2017-03-26 12:59:01 +0100

    Fixed formatting

--- src/controllers/error-controller.ts
+++ src/controllers/error-controller.ts
@@ -13,11 +13,10 @@ export class ErrorController extends Controller {
 	 * Creates an instance
 	 */
     constructor( e: express.Express ) {
-        super(null);
+        super( null );
 
         // Handle all errors the same way
         e.use( function( err: Error, req: express.Request, res: express.Response ) {
-            req; // Suppress compiler warning
             res.setHeader( 'Content-Type', 'application/json' );
             return res.end( JSON.stringify( <def.IResponse>{ message: err.toString(), error: true } ) );
         } );
