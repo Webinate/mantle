5984544b3c8262adb455c2ec8e1a6093b2e299c3
Mathew Henson <mat@webinate.net>
2017-04-04 20:23:23 +0100

    Creating a wrapper class for agents

--- test/tests/header.js
+++ test/tests/header.js
@@ -3,6 +3,84 @@ let fs = require( 'fs' );
 let yargs = require( "yargs" );
 let args = yargs.argv;
 
+let admin;
+
+class Agent {
+    constructor( agent, cookie ) {
+        this.agent = agent;
+        this.cookie = cookie;
+        this.setDefaults();
+    }
+
+    setDefaults() {
+        this.code = 200;
+        this.accepts = 'application/json';
+        this.contentType = /json/;
+    }
+
+    code( val ) {
+        this.code = val;
+        return this;
+    }
+
+    accepts( val ) {
+        this.accepts = val;
+        return this;
+    }
+
+    contentType( val ) {
+        this.contentType = val;
+        return this;
+    }
+
+    go( url, data, type ) {
+        return new Promise(( resolve, reject ) => {
+            let req = null;
+            if (type === 'post')
+                req = this.agent.post( url );
+            else if (type === 'delete')
+                req = this.agent.delete( url );
+            else if (type === 'put')
+                req = this.agent.put( url );
+            else
+                req = this.agent.get( url );
+
+            req.set( 'Accept', this.accepts );
+            req.expect( this.code )
+            req.expect( 'Content-Type', this.contentType )
+            if (data)
+                req.send( data )
+
+            if ( this.cookie )
+                req.set( 'Cookie', this.cookie )
+
+            req.end(( err, res ) => {
+                if ( err )
+                    return reject( err );
+
+                this.setDefaults()
+                return ( resolve( res ) );
+            } );
+        });
+    }
+
+    post(url, data) {
+        return this.go(url, data, 'post');
+    }
+
+    delete(url, data) {
+        return this.go(url, data, 'delete');
+    }
+
+    put(url, data) {
+        return this.go(url, data, 'put');
+    }
+
+    get(url, data) {
+        return this.go(url );
+    }
+}
+
 /**
  * A class for managing the tests
  */
@@ -19,8 +97,8 @@ class TestManager {
         };
 
         this.config = JSON.parse( fs.readFileSync( args.config ) );
-        this.serverConfig = this.config.servers[ parseInt( args.server ) ];
-        this.agent = test.httpAgent( "http://" + this.serverConfig.host + ":" + this.serverConfig.portHTTP );
+        const serverConfig = this.config.servers[ parseInt( args.server ) ];
+        this.agent = test.httpAgent( "http://" + serverConfig.host + ":" + serverConfig.portHTTP );
     }
 
     post( url, json, who = 'admin', code = 200 ) {
@@ -90,6 +168,10 @@ class TestManager {
         this.cookies[ who ] = resp.headers[ "set-cookie" ][ 0 ].split( ";" )[ 0 ];
     }
 
+    async createAgent(  ) {
+
+    }
+
     /**
      * Initialize the manager
      */
@@ -98,6 +180,9 @@ class TestManager {
             const config = this.config;
             const resp = await this.post( '/auth/login', { username: config.adminUser.username, password: config.adminUser.password } );
             this.cookies.admin = resp.headers[ "set-cookie" ][ 0 ].split( ";" )[ 0 ];
+
+            const serverConfig = this.config.servers[ parseInt( args.server ) ];
+            admin = new Agent( test.httpAgent( "http://" + serverConfig.host + ":" + serverConfig.portHTTP ), this.cookies.admin );
         }
         catch ( exp ) {
             console.log( exp.toString() )
@@ -117,3 +202,4 @@ class TestManager {
 };
 
 exports.TestManager = TestManager;
+exports.admin = admin;
