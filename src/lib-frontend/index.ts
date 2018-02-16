b054a40e54b5c937a017361e7cf243c76ff39647
Mathew Henson <mat@webinate.net>
2018-02-16 16:10:04 +0000

    Exposed auth functions to frontend api

--- src/lib-frontend/index.ts
+++ src/lib-frontend/index.ts
@@ -1,9 +1,11 @@
 import * as http from './http-clients';
 import * as posts from './posts';
 import * as users from './users';
+import * as auth from './auth';
 
 export {
   http,
   posts,
-  users
+  users,
+  auth
 }
