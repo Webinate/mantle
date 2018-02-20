95b26f703cc5019222cba10ee28d75e0c16d3332
Mathew Henson <mat@webinate.net>
2018-02-20 16:31:45 +0000

    Added a comments api for frontend clients

---
+++ src/lib-frontend/comments.ts
@@ -0,0 +1,51 @@
+import { getJson, makeQueryString, del, putJson, postJson, apiUrl } from './http-clients';
+import { Page } from '../lib/types/tokens/standard-tokens';
+import { IComment } from '..';
+
+const rootPath = `${apiUrl}/comments`;
+
+export type GetAllOptions = {
+  visibility: 'all' | 'public' | 'private';
+  sort: string;
+  user: string;
+  index: number;
+  depth: number;
+  limit: number;
+  expanded: boolean;
+  keyword: string;
+  parentId: string;
+  sortOrder: 'asc' | 'desc';
+  verbose: boolean;
+}
+
+export async function getAll( options: Partial<GetAllOptions> ) {
+  const page = await getJson<Page<IComment>>( rootPath + makeQueryString( options ) );
+  return page;
+}
+
+export async function getAllFromParent( parentId: string, options: Partial<GetAllOptions> ) {
+  const page = await getJson<Page<IComment>>( `${apiUrl}/nested-comments/${parentId}` + makeQueryString( options ) );
+  return page;
+}
+
+export async function getAllFromUser( user: string, options: Partial<GetAllOptions> ) {
+  const page = await getJson<Page<IComment>>( `${apiUrl}/users/${user}/comments` + makeQueryString( options ) );
+  return page;
+}
+
+export async function getOne( id: string, options: { expanded?: boolean; verbose?: boolean; } ) {
+  const page = await getJson<IComment>( `${rootPath}/${id}${options.verbose ? makeQueryString( { verbose: true } ) : ''}` );
+  return page;
+}
+
+export function remove( id: string ) {
+  return del( `${rootPath}/${id}` );
+}
+
+export function update( id: string, token: Partial<IComment> ) {
+  return putJson<IComment>( `${rootPath}/${id}`, token );
+}
+
+export function create( postId: string, token: Partial<IComment>, parentId?: string ) {
+  return postJson<IComment>( `${apiUrl}/posts/${postId}/comments${parentId ? '/' + parentId : ''}`, token );
+}
--- src/lib-frontend/index.ts
+++ src/lib-frontend/index.ts
@@ -1,11 +1,13 @@
 import * as http from './http-clients';
 import * as posts from './posts';
 import * as users from './users';
+import * as comments from './comments';
 import * as auth from './auth';
 
 export {
   http,
   posts,
   users,
-  auth
+  auth,
+  comments
 }
