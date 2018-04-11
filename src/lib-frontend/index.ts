05b53c289bc91773f9b9097089ca27633d3e0511
Mathew Henson <mat@webinate.net>
2018-04-11 21:17:35 +0100

    Added category client helper to frontend index

--- src/lib-frontend/index.ts
+++ src/lib-frontend/index.ts
@@ -2,11 +2,13 @@ import * as http from './http-clients';
 import * as posts from './posts';
 import * as users from './users';
 import * as comments from './comments';
+import * as categories from './categories';
 import * as auth from './auth';
 
 export {
   http,
   posts,
+  categories,
   users,
   auth,
   comments
