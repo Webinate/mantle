32f4d74d3cc66c1b83ebdbc0730175714237b328
Mathew Henson <mat@webinate.net>
2018-04-11 20:06:20 +0100

    Created category helper functions

---
+++ src/lib-frontend/categories.ts
@@ -0,0 +1,24 @@
+import { getJson, makeQueryString, del, postJson, apiUrl } from './http-clients';
+import { ICategory } from '../lib/types/models/i-category';
+import { Page } from '../lib/types/tokens/standard-tokens';
+
+const rootPath = `${apiUrl}/categories`;
+
+export type GetAllOptions = {
+  index: number;
+  limit: number;
+  verbose: boolean;
+}
+
+export async function getAll( options: Partial<GetAllOptions> ) {
+  const page = await getJson<Page<ICategory>>( rootPath + makeQueryString( options ) );
+  return page;
+}
+
+export function remove( id: string ) {
+  return del( `${rootPath}/${id}` );
+}
+
+export function create( token: Partial<ICategory> ) {
+  return postJson<ICategory>( rootPath, token );
+}
