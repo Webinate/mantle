5034d7b9ab8847b059bcbba7ade7c67205b381ec
Mathew Henson <mat@webinate.net>
2018-04-19 00:25:57 +0100

    Added category post check for regular user permissions

--- test/tests/categories/1-create-category.ts
+++ test/tests/categories/1-create-category.ts
@@ -4,18 +4,24 @@ import { ICategory, IConfig, Page } from '../../../src';
 import header from '../header';
 import Agent from '../agent';
 
-let guest: Agent, admin: Agent, config: IConfig, numComments: number;
+let guest: Agent, admin: Agent, user1: Agent, config: IConfig, numComments: number;
 
 describe( '1. Testing creation of comments', function() {
 
   before( function() {
     guest = header.users.guest;
     admin = header.users.admin;
+    user1 = header.users.user1;
     config = header.config;
   } )
 
   it( 'did not create a category when not logged in', async function() {
     const resp = await guest.post( `/api/categories`, { title: 'Test' } as ICategory );
     assert.equal( resp.status, 401 );
+  } )
+
+  it( 'did not create a category for a regular user', async function() {
+    const resp = await user1.post( `/api/categories`, { title: 'Test' } as ICategory );
+    assert.equal( resp.status, 403 );
   } )
 } );
