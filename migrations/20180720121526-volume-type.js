83e8f2b239cea96f2537a8c2b9fce863bce2c0be
Mathew Henson <mat@webinate.net>
2018-07-22 10:52:30 +0100

    Added volume script that defaults existing volumes to Google

--- migrations/20180720121526-volume-type.js
+++ migrations/20180720121526-volume-type.js
@@ -1,15 +1,20 @@
 'use strict';
 
 module.exports = {
+  up: async function( db, next ) {
+    try {
+      const volumes = await db.collection( 'volumes' );
+      volumes.update( {}, { $set: { type: 'google' } } );
+    }
+    catch ( err ) {
+      console.error( `An error ocurred. Error Stack: ${err.stack}` )
+      return next( err )
+    }
 
-  up(db, next) {
-    // TODO write your migration here
-    next();
+    next()
   },
 
-  down(db, next) {
-    // TODO write the statements to rollback your migration (if possible)
-    next();
+  down: async function( db, next ) {
+    next()
   }
-
 };
