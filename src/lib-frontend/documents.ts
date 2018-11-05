2569029b2feecabbed6c212c0596473cad130699
Mathew Henson <mat@webinate.net>
2018-11-05 13:58:06 +0000

    Added clients for templates & documents

---
+++ src/lib-frontend/documents.ts
@@ -0,0 +1,35 @@
+import { getJson, putJson, postJson, apiUrl, del } from './http-clients';
+import { Page } from '../types/tokens/standard-tokens';
+import { IDocument } from '../types/models/i-document';
+import { IDraftElement } from '../types/models/i-draft-elements';
+
+const rootPath = `${apiUrl}/documents`;
+
+export async function getAll() {
+  const page = await getJson<Page<IDocument<'client'>>>( rootPath );
+  return page;
+}
+
+export async function getOne( id: string ) {
+  const doc = await getJson<IDocument<'client'>>( `${rootPath}/${id}` );
+  return doc;
+}
+
+export async function setTemplate( docId: string, templateId: string ) {
+  const doc = await putJson<IDocument<'client'>>( `${rootPath}/${docId}/set-template/${templateId}`, {} );
+  return doc;
+}
+
+export async function addElement( docId: string, elm: Partial<IDraftElement<'client'>> ) {
+  const doc = await postJson<IDraftElement<'client'>>( `${rootPath}/${docId}/elements`, elm );
+  return doc;
+}
+
+export async function editElement( docId: string, elementId: string, elm: Partial<IDraftElement<'client'>> ) {
+  const doc = await putJson<IDraftElement<'client'>>( `${rootPath}/${docId}/elements/${elementId}`, elm );
+  return doc;
+}
+
+export function removeElement( docId: string, elementId: string ) {
+  return del( `${rootPath}/${docId}/elements/${elementId}` );
+}
