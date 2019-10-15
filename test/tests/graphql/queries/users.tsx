7e0fda97648e0edf40cfb68ac5bd1eae06997537
Mathew <mat@salteese.com>
2019-10-15 17:25:50 +0100

    Added tests for creating and removing users

--- test/tests/graphql/queries/users.tsx
+++ test/tests/graphql/queries/users.tsx
@@ -2,11 +2,41 @@ import gql from '../../../../src/utils/gql';
 import { userFragment } from '../fragments';
 
 export const GET_USER = gql`
-  query getUser {
-    getUser {
+  query getUser($username: String) {
+    getUser(username: $username) {
       ...UserFields
     }
   }
 
   ${userFragment}
+`;
+
+export const GET_USERS = gql`
+  query getUsers($search: String, $index: Int, $limit: Int) {
+    getUsers(search: $search, index: $index, limit: $limit) {
+      count
+      index
+      data {
+        ...UserFields
+      }
+    }
+  }
+
+  ${userFragment}
+`;
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!, $privileges: UserPriviledgeEnumType) {
+    createUser(username: $username, email: $email, password: $password, privileges: $privileges) {
+      ...UserFields
+    }
+  }
+
+  ${userFragment}
+`;
+
+export const REMOVE_USER = gql`
+  mutation removeUser($username: String!) {
+    removeUser(username: $username)
+  }
 `;
---
+++ test/tests/graphql/user/delete-user.ts
@@ -0,0 +1,65 @@
+import * as assert from 'assert';
+import Agent from '../../agent';
+import header from '../../header';
+import { IUserEntry, Page } from '../../../../src';
+import { randomString } from '../../utils';
+import { REMOVE_USER, GET_USERS, CREATE_USER } from '../queries/users';
+
+let numUsers: number,
+  agent: Agent,
+  testUserName = `test${randomString(6)}`,
+  testUserEmail = `test${randomString(6)}@fancy.com`;
+
+describe('[GQL] Testing deleting users', function() {
+  it('did get the number of users', async function() {
+    const {
+      data: { count }
+    } = await header.admin.graphql<Page<IUserEntry<'expanded'>>>(GET_USERS);
+    numUsers = count;
+  });
+
+  it('did not allow a regular user to remove another user', async function() {
+    const { errors } = await header.user1.graphql<boolean>(REMOVE_USER, {
+      username: header.user2.username
+    });
+    assert.deepEqual(errors[0].message, 'You do not have permission');
+  });
+
+  it(`did create & login regular user ${testUserName} with valid details`, async function() {
+    const { data: newUser } = await header.admin.graphql<IUserEntry<'expanded'>>(CREATE_USER, {
+      username: testUserName,
+      password: 'password',
+      email: testUserEmail,
+      privileges: 'regular'
+    });
+
+    assert(newUser._id);
+
+    const newAgent = await header.createUser(testUserName, 'password', testUserEmail);
+    agent = newAgent;
+  });
+
+  it('did increment the number of users', async function() {
+    const {
+      data: { count }
+    } = await header.admin.graphql<Page<IUserEntry<'expanded'>>>(GET_USERS);
+
+    assert(count - 1 === numUsers);
+  });
+
+  it('did allow the regular user to delete its own account', async function() {
+    const { data: removed } = await agent.graphql<boolean>(REMOVE_USER, {
+      username: testUserName
+    });
+
+    assert(removed);
+  });
+
+  it('did have the same number of users as before the tests started', async function() {
+    const {
+      data: { count }
+    } = await header.admin.graphql<Page<IUserEntry<'expanded'>>>(GET_USERS);
+
+    assert(count === numUsers);
+  });
+});
