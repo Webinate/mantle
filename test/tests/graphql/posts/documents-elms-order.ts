37bb474ffa83ea8f81604d9fa6dcfa461e99f6b5
Mathew <mat@salteese.com>
2020-05-20 23:33:02 +0100

    implementde more post tests

--- test/tests/graphql/posts/documents-elms-order.ts
+++ test/tests/graphql/posts/documents-elms-order.ts
@@ -1,115 +1,117 @@
-// import * as assert from 'assert';
-// import { IPost, IDocument, IUserEntry, IDraftElement } from '../../../../src';
-// import ControllerFactory from '../../../../src/core/controller-factory';
-// import { randomString } from '../../utils';
-// import header from '../../header';
+import * as assert from 'assert';
+import { IPost, IDocument, IUserEntry, IDraftElement } from '../../../../src';
+import ControllerFactory from '../../../../src/core/controller-factory';
+import { randomString } from '../../utils';
+import header from '../../header';
+import { AddElementInput } from '../../../../src/graphql/models/element-type';
+import { ElementType } from '../../../../src/core/enums';
+import { ADD_DOC_ELEMENT, GET_DOCUMENT, REMOVE_DOC_ELEMENT } from '../../../../src/graphql/client/requests/documents';
 
-// let post: IPost<'expanded'>, document: IDocument<'expanded'>, user1: IUserEntry<'expanded'>;
+let post: IPost<'server'>;
 
-// let firstElm: IDraftElement<'expanded'>;
-// let secondElm: IDraftElement<'expanded'>;
+let firstElm: IDraftElement<'expanded'>;
+let secondElm: IDraftElement<'expanded'>;
 
-// describe('[GQL] Testing the order of document elements: ', function() {
-//   before(async function() {
-//     const posts = ControllerFactory.get('posts');
-//     const users = ControllerFactory.get('users');
-//     user1 = (await users.getUser({ username: 'user1' })) as IUserEntry<'expanded'>;
+describe('[GQL] Testing the order of document elements: ', function() {
+  before(async function() {
+    const posts = ControllerFactory.get('posts');
+    const users = ControllerFactory.get('users');
+    const user1 = (await users.getUser({ username: 'user1' })) as IUserEntry<'server'>;
 
-//     // Create post and comments
-//     post = (await posts.create({
-//       author: user1!._id,
-//       slug: randomString(),
-//       title: 'Temp Post',
-//       public: true
-//     })) as IPost<'expanded'>;
+    // Create post and comments
+    post = await posts.create({
+      author: user1!._id,
+      slug: randomString(),
+      title: 'Temp Post',
+      public: true
+    });
+  });
 
-//     document = post.document;
-//   });
+  after(async function() {
+    const posts = ControllerFactory.get('posts');
+    await posts.removePost(post._id);
+  });
 
-//   after(async function() {
-//     const posts = ControllerFactory.get('posts');
-//     await posts.removePost(post._id);
-//   });
+  it('did add a new elements and each is added to the end of the order array', async function() {
+    const { data: elm1 } = await header.admin.graphql<IDraftElement<'expanded'>>(ADD_DOC_ELEMENT, {
+      docId: post.document,
+      token: new AddElementInput({
+        type: ElementType.paragraph,
+        html: '<p>A</p>'
+      })
+    });
 
-//   it('did add a new elements and each is added to the end of the order array', async function() {
-//     const { data: elm1 } = await header.user1.graphql<IDraftElement<'expanded'>>(
-//       `mutation { addDocElement(id: "${
-//         document._id
-//       }", token: { type: ElmParagraph, html: "<p>A</p>", }) { _id, html } }`
-//     );
-//     const { data: elm2 } = await header.user1.graphql<IDraftElement<'expanded'>>(
-//       `mutation { addDocElement(id: "${
-//         document._id
-//       }", token: { type: ElmParagraph, html: "<p>B</p>", }) { _id, html } }`
-//     );
+    const { data: elm2 } = await header.admin.graphql<IDraftElement<'expanded'>>(ADD_DOC_ELEMENT, {
+      docId: post.document,
+      token: new AddElementInput({
+        type: ElementType.paragraph,
+        html: '<p>B</p>'
+      })
+    });
 
-//     firstElm = elm1;
-//     secondElm = elm2;
+    firstElm = elm1;
+    secondElm = elm2;
 
-//     const { data: doc } = await header.user1.graphql<IDocument<'expanded'>>(
-//       `{ getDocument(id: "${document._id}") { elementsOrder } }`
-//     );
+    const { data: doc } = await header.admin.graphql<IDocument<'expanded'>>(GET_DOCUMENT, { id: post.document });
 
-//     assert.equal(doc.elementsOrder[1], firstElm._id);
-//     assert.equal(doc.elementsOrder[2], secondElm._id);
-//   });
+    assert.equal(doc.elementsOrder[1], firstElm._id);
+    assert.equal(doc.elementsOrder[2], secondElm._id);
+  });
 
-//   it('did remove an element and the element id was removed from the order array', async function() {
-//     const { data: elementRemoved } = await header.user1.graphql<boolean>(
-//       `mutation{ removeDocElement(docId: "${document._id}", elementId: "${firstElm._id}") }`
-//     );
+  it('did remove an element and the element id was removed from the order array', async function() {
+    const { data: elementRemoved } = await header.admin.graphql<boolean>(REMOVE_DOC_ELEMENT, {
+      docId: post.document,
+      elementId: firstElm._id
+    });
+    assert.deepEqual(elementRemoved, true);
 
-//     assert.deepEqual(elementRemoved, true);
+    const { data: doc } = await header.admin.graphql<IDocument<'expanded'>>(GET_DOCUMENT, { id: post.document });
+    assert.equal(doc.elementsOrder[1], secondElm._id);
+    assert.equal(doc.elementsOrder.length, 2);
+  });
 
-//     const { data: doc } = await header.user1.graphql<IDocument<'expanded'>>(
-//       `{ getDocument(id: "${document._id}") { elementsOrder } }`
-//     );
+  it('did add a new element at an index and the order array was correct', async function() {
+    const { data: newElement } = await header.admin.graphql<IDraftElement<'expanded'>>(ADD_DOC_ELEMENT, {
+      docId: post.document,
+      index: 1,
+      token: new AddElementInput({
+        type: ElementType.paragraph,
+        html: '<p>C</p>'
+      })
+    });
 
-//     assert.equal(doc.elementsOrder[1], secondElm._id);
-//     assert.equal(doc.elementsOrder.length, 2);
-//   });
+    const { data: doc } = await header.admin.graphql<IDocument<'expanded'>>(GET_DOCUMENT, { id: post.document });
 
-//   it('did add a new element at an index and the order array was correct', async function() {
-//     const { data: newElement } = await header.user1.graphql<IDraftElement<'expanded'>>(
-//       `mutation { addDocElement(id: "${
-//         document._id
-//       }", index: 1, token: { type: ElmParagraph, html: "<p>C</p>", }) { _id, html } }`
-//     );
+    assert.equal(doc.elementsOrder[1], newElement._id);
+    assert.equal(doc.elementsOrder[2], secondElm._id);
+    assert.equal(doc.elementsOrder.length, 3);
+  });
 
-//     const { data: doc } = await header.user1.graphql<IDocument<'expanded'>>(
-//       `{ getDocument(id: "${document._id}") { elementsOrder } }`
-//     );
+  it('did add an element at an index -1 to the end of the order array', async function() {
+    const { data: newElement } = await header.admin.graphql<IDraftElement<'expanded'>>(ADD_DOC_ELEMENT, {
+      docId: post.document,
+      index: -1,
+      token: new AddElementInput({
+        type: ElementType.paragraph,
+        html: '<p>D</p>'
+      })
+    });
 
-//     assert.equal(doc.elementsOrder[1], newElement._id);
-//     assert.equal(doc.elementsOrder[2], secondElm._id);
-//     assert.equal(doc.elementsOrder.length, 3);
-//   });
+    const { data: doc } = await header.admin.graphql<IDocument<'expanded'>>(GET_DOCUMENT, { id: post.document });
+    assert.equal(doc.elementsOrder[3], newElement._id);
+  });
 
-//   it('did add an element at an index -1 to the end of the order array', async function() {
-//     const { data: newElement } = await header.user1.graphql<IDraftElement<'expanded'>>(
-//       `mutation { addDocElement(id: "${
-//         document._id
-//       }", index: -1, token: { type: ElmParagraph, html: "<p>D</p>", }) { _id, html } }`
-//     );
+  it('did add an element at an index 100 to the end of the order array', async function() {
+    const { data: newElement } = await header.admin.graphql<IDraftElement<'expanded'>>(ADD_DOC_ELEMENT, {
+      docId: post.document,
+      index: 100,
+      token: new AddElementInput({
+        type: ElementType.paragraph,
+        html: '<p>E</p>'
+      })
+    });
 
-//     const { data: doc } = await header.user1.graphql<IDocument<'expanded'>>(
-//       `{ getDocument(id: "${document._id}") { elementsOrder } }`
-//     );
-
-//     assert.equal(doc.elementsOrder[3], newElement._id);
-//   });
-
-//   it('did add an element at an index 100 to the end of the order array', async function() {
-//     const { data: newElement } = await header.user1.graphql<IDraftElement<'expanded'>>(
-//       `mutation { addDocElement(id: "${
-//         document._id
-//       }", index: 100, token: { type: ElmParagraph, html: "<p>E</p>", }) { _id, html } }`
-//     );
-
-//     const { data: doc } = await header.user1.graphql<IDocument<'expanded'>>(
-//       `{ getDocument(id: "${document._id}") { elementsOrder } }`
-//     );
-
-//     assert.equal(doc.elementsOrder[4], newElement._id);
-//   });
-// });
+    const { data: doc } = await header.admin.graphql<IDocument<'expanded'>>(GET_DOCUMENT, { id: post.document });
+    assert.equal(doc.elementsOrder[4], newElement._id);
+  });
+});
--- test/tests/graphql/posts/documents-elms-remove.ts
+++ test/tests/graphql/posts/documents-elms-remove.ts
@@ -1,88 +1,89 @@
-// import * as assert from 'assert';
-// import { IPost, IDocument, IUserEntry } from '../../../../src';
-// import ControllerFactory from '../../../../src/core/controller-factory';
-// import { randomString } from '../../utils';
-// import header from '../../header';
-// import { IDraft } from '../../../../src/types/models/i-draft';
+import * as assert from 'assert';
+import { IPost, IDocument, IUserEntry } from '../../../../src';
+import ControllerFactory from '../../../../src/core/controller-factory';
+import { randomString } from '../../utils';
+import header from '../../header';
+import { REMOVE_DOC_ELEMENT, GET_DOCUMENT } from '../../../../src/graphql/client/requests/documents';
 
-// let post: IPost<'expanded'>, document: IDocument<'expanded'>, user1: IUserEntry<'expanded'>;
+let post: IPost<'server'>, document: IDocument<'expanded'>;
 
-// describe('[GQL] Testing the deletion of document elements: ', function() {
-//   before(async function() {
-//     const posts = ControllerFactory.get('posts');
-//     const users = ControllerFactory.get('users');
-//     user1 = (await users.getUser({ username: 'user1' })) as IUserEntry<'expanded'>;
+describe('[GQL] Testing the deletion of document elements: ', function() {
+  before(async function() {
+    const posts = ControllerFactory.get('posts');
+    const users = ControllerFactory.get('users');
+    const user1 = (await users.getUser({ username: 'user1' })) as IUserEntry<'server'>;
 
-//     // Create post and comments
-//     post = (await posts.create({
-//       author: user1!._id,
-//       slug: randomString(),
-//       title: 'Temp Post',
-//       public: true
-//     })) as IPost<'expanded'>;
+    // Create post and comments
+    post = await posts.create({
+      author: user1!._id,
+      slug: randomString(),
+      title: 'Temp Post',
+      public: true
+    });
 
-//     document = post.document;
-//   });
+    const resp = await header.admin.graphql<IDocument<'expanded'>>(GET_DOCUMENT, { id: post.document });
+    document = resp.data;
+  });
 
-//   after(async function() {
-//     const posts = ControllerFactory.get('posts');
-//     await posts.removePost(post._id);
-//   });
+  after(async function() {
+    const posts = ControllerFactory.get('posts');
+    await posts.removePost(post._id);
+  });
 
-//   it('did not remove an element with a bad document id', async function() {
-//     const { errors } = await header.guest.graphql<boolean>(
-//       `mutation{ removeDocElement(docId: "bad", elementId: "bad") }`
-//     );
+  it('did not remove an element with a bad document id', async function() {
+    const { errors } = await header.guest.graphql<boolean>(REMOVE_DOC_ELEMENT, { docId: 'bad', elementId: 'bad' });
 
-//     assert.deepEqual(
-//       errors[0].message,
-//       'Expected type ObjectId!, found "bad"; ObjectId must be a single String of 24 hex characters'
-//     );
-//   });
+    assert.deepEqual(
+      errors![0].message,
+      'Variable "$docId" got invalid value "bad"; Expected type ObjectId. Argument passed in must be a single String of 12 bytes or a string of 24 hex characters'
+    );
 
-//   it('did not remove an element on a document that doesnt exist', async function() {
-//     const { errors } = await header.user1.graphql<boolean>(
-//       `mutation{ removeDocElement(docId: "123456789012345678901234", elementId: "123456789012345678901234") }`
-//     );
-//     assert.deepEqual(errors[0].message, 'Document not found');
-//   });
+    assert.deepEqual(
+      errors![1].message,
+      'Variable "$elementId" got invalid value "bad"; Expected type ObjectId. Argument passed in must be a single String of 12 bytes or a string of 24 hex characters'
+    );
+  });
 
-//   it('did not delete an element on a document that doesnt exist', async function() {
-//     const { errors } = await header.user1.graphql<boolean>(
-//       `mutation{ removeDocElement(docId: "${document._id}", elementId: "123456789012345678901234") }`
-//     );
-//     assert.deepEqual(errors[0].message, 'Could not find resource');
-//   });
+  it('did not remove an element on a document that doesnt exist', async function() {
+    const { errors } = await header.admin.graphql<boolean>(REMOVE_DOC_ELEMENT, {
+      docId: '123456789012345678901234',
+      elementId: '123456789012345678901234'
+    });
+    assert.deepEqual(errors![0].message, 'Document not found');
+  });
 
-//   it('did not allow a guest to remove an element', async function() {
-//     const { errors } = await header.guest.graphql<boolean>(
-//       `mutation{ removeDocElement(docId: "${document._id}", elementId: "${document.elements[0]._id}") }`
-//     );
+  it('did not delete an element on a document that doesnt exist', async function() {
+    const { errors } = await header.admin.graphql<boolean>(REMOVE_DOC_ELEMENT, {
+      docId: post.document,
+      elementId: '123456789012345678901234'
+    });
+    assert.deepEqual(errors![0].message, 'Could not find resource');
+  });
 
-//     assert.deepEqual(errors[0].message, 'Authentication Error');
-//   });
+  it('did not allow a guest to remove an element', async function() {
+    const { errors } = await header.guest.graphql<boolean>(REMOVE_DOC_ELEMENT, {
+      docId: post.document,
+      elementId: document.elements[0]._id
+    });
+    assert.deepEqual(errors![0].message, `Access denied! You don't have permission for this action!`);
+  });
 
-//   it('did not allow another user to remove an element', async function() {
-//     const { errors } = await header.user2.graphql<boolean>(
-//       `mutation{ removeDocElement(docId: "${document._id}", elementId: "${document.elements[0]._id}") }`
-//     );
+  // it('did not allow another user to remove an element', async function() {
+  //   const { errors } = await header.user2.graphql<boolean>( REMOVE_DOC_ELEMENT, { docId: post.document, elementId: document.elements[0] });
+  //   assert.deepEqual(errors![0].message, 'You do not have permission');
+  // });
 
-//     assert.deepEqual(errors[0].message, 'You do not have permission');
-//   });
+  it('did allow an admin user to delete the unit', async function() {
+    const { data: fileRemoved } = await header.admin.graphql<boolean>(REMOVE_DOC_ELEMENT, {
+      docId: post.document,
+      elementId: document.elements[0]._id
+    });
+    assert.deepEqual(fileRemoved, true);
+  });
 
-//   it('did allow a regular user to delete the unit', async function() {
-//     const { data: fileRemoved } = await header.user1.graphql<boolean>(
-//       `mutation{ removeDocElement(docId: "${document._id}", elementId: "${document.elements[0]._id}") }`
-//     );
+  it('did update the draft html', async function() {
+    const { data: doc } = await header.admin.graphql<IDocument<'expanded'>>(GET_DOCUMENT, { id: post.document });
 
-//     assert.deepEqual(fileRemoved, true);
-//   });
-
-//   it('did update the draft html', async function() {
-//     const { data: doc } = await header.user1.graphql<IDocument<'expanded'>>(
-//       `{ getDocument(id: "${document._id}") { html } }`
-//     );
-
-//     assert.deepEqual(doc.html, undefined);
-//   });
-// });
+    assert.ok(Object.keys(doc.html).length === 0 && doc.html.constructor === Object);
+  });
+});
